61312e175249ae34435fa80ce02247b1d6ca2980
EddyJo <khjo@nhnsoft.com>
2021-04-07 22:11:42 +0900

    feat: method reference 퀴즈

---
+++ src/main/java/kr/co/eddy/java/in/action/dish/Dish.java
@@ -0,0 +1,8 @@
+package kr.co.eddy.java.in.action.dish;
+
+public class Dish {
+    private final String name;
+    private final boolean vegeterian;
+    private final int calories;
+    private final Type type;
+}
--- src/test/java/kr/co/eddy/java/in/action/method/reference/MethodReferenceTest.java
+++ src/test/java/kr/co/eddy/java/in/action/method/reference/MethodReferenceTest.java
@@ -2,13 +2,17 @@ package kr.co.eddy.java.in.action.method.reference;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.list;
 
 public class MethodReferenceTest {
 
@@ -26,5 +30,19 @@ public class MethodReferenceTest {
         assertThat(stringToIntMethodReference.applyAsInt("2")).isEqualTo(stringToIntOriginal.applyAsInt("2"));
     }
 
+    @Test
+    void BiPredicateTest() {
+        BiPredicate<List<String>, String> containsOriginal = (list, element) -> list.contains(element);
+        BiPredicate<List<String>, String> containsMethodReference = List::contains;
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("안녕");
+        assertThat(containsMethodReference.test(list1, "메롱")).isFalse();
+        assertThat(containsOriginal.test(list1, "메롱")).isFalse();
+
+        assertThat(containsMethodReference.test(list1, "안녕")).isTrue();
+        assertThat(containsOriginal.test(list1, "안녕")).isTrue();
+
+    }
 
 }

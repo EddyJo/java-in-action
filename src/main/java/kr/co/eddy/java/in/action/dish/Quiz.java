debc932b7e2f3ff1fc7e244c2c151ba5680bfba6
EddyJo <khjo@nhnsoft.com>
2021-04-08 22:40:08 +0900

    feat: stream filter

--- src/main/java/kr/co/eddy/java/in/action/dish/Quiz.java
+++ src/main/java/kr/co/eddy/java/in/action/dish/Quiz.java
@@ -10,10 +10,21 @@ public class Quiz {
     public static void main(String[] args) {
 
         //외부반복 -> 내부반복
+        System.out.println("======== 외부반복 -> 내부반복 ========");
         List<Dish> highCaloriesDish = new Menu().menu.stream()
                                                      .filter(dish -> dish.getCalories() > 300)
                                                      .collect(toList());
 
         System.out.println(highCaloriesDish);
+
+        //Quiz 5-1. 스트림을 이용하여 처음 등장하는 두 고기 요리를 필터링하시오.
+        System.out.println("======== Quiz 5-1 ========");
+        System.out.println(new Menu().menu.stream()
+                                          .filter(dish -> dish.getType() == Dish.Type.MEAT)
+                                          .limit(2)
+                                          .collect(toList())
+                          );
+
+
     }
 }
